9047ce396eb4e0901aad56a54ba0219c77d6f052
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2026-07-22 19:54:18 +0530

    chore: initial commit

---
+++ backend/src/main/java/com/example/demo/BackendApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+// @EnableJpaRepositories(basePackages = "com.example.repository")
+// @EntityScan(basePackages = "com.example.entity")
+// @ComponentScan(basePackages = "com.example")
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+}
